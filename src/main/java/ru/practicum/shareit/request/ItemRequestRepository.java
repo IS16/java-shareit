833fc363adf3b03d1f9fb7671f08c129ea2ff428
IS16 <sarkisov775@yandex.ru>
2024-01-04 15:53:31 +0300

    some style changes

--- src/main/java/ru/practicum/shareit/request/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestRepository.java
@@ -10,6 +10,8 @@ import java.util.List;
 
 public interface ItemRequestRepository extends PagingAndSortingRepository<ItemRequest, Long> {
     List<ItemRequest> findAllByRequestorId(Long requestorId, Sort sort);
+
     Page<ItemRequest> findAllByRequestorIdNot(Long userId, Pageable pageable);
+
     List<ItemRequest> findAllByRequestorIdNotOrderByCreatedDesc(Long userId);
 }
